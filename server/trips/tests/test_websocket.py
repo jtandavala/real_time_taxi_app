e47530dda892ce3278601df181fb0d9b2cdadad4
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-09-19 20:21:42 +0100

    :sparkles: (Websocket) creating a trip

--- server/trips/tests/test_websocket.py
+++ server/trips/tests/test_websocket.py
@@ -30,10 +30,10 @@ def create_user(username, password, group='rider'):
     
     
 
-
 @pytest.mark.asyncio
 @pytest.mark.django_db(transaction=True)
 class TestWebSocket:
+
     async def test_can_connect_to_server(self, settings):
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
         _, access = await create_user(
@@ -95,4 +95,32 @@ class TestWebSocket:
         await channel_layer.group_send('drivers', message=message)
         response = await communicator.receive_json_from()
         assert response == message
+        await communicator.disconnect()
+
+    async def test_request_trip(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        user, access = await create_user(
+            'test.user@example.com', 'pAssw0rd', 'rider'
+        )
+        communicator = WebsocketCommunicator(
+            application=application,
+            path=f'/taxi/?token={access}'
+        )
+        await communicator.connect()
+        await communicator.send_json_to({
+            'type': 'create.trip',
+            'data': {
+                'pick_up_address': '123 Main Street',
+                'drop_off_address': '456 Piney Road',
+                'rider': user.id,
+            },
+        })
+        response = await communicator.receive_json_from()
+        response_data = response.get('data')
+        assert response_data['id'] is not None
+        assert response_data['pick_up_address'] == '123 Main Street'
+        assert response_data['drop_off_address'] == '456 Piney Road'
+        assert response_data['status'] == 'REQUESTED'
+        assert response_data['rider']['username'] == user.username
+        assert response_data['driver'] is None
         await communicator.disconnect()
