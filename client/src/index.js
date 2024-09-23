3b1ffdb3d53c6eb818aa90179ca5a0e0a9f50340
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-09-23 20:26:18 +0100

    :sparkles: (Route) add react route

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Landing from "./components/Landing";
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+
+        <Route index element={<Landing />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="log-in" element={<LogIn />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
