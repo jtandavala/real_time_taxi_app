42324674fb1ee5ddb6f09b2224a39f2bd5a6013a
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-09-24 19:59:14 +0100

    :sparkles: (Bootstrap) add React bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,5 @@
+import "bootswatch/dist/lumen/bootstrap.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -14,11 +16,11 @@ root.render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-
-        <Route index element={<Landing />} />
-        <Route path="sign-up" element={<SignUp />} />
-        <Route path="log-in" element={<LogIn />} />
+        <Route path="/" element={<App />}>
+          <Route index element={<Landing />} />
+          <Route path="sign-up" element={<SignUp />} />
+          <Route path="log-in" element={<LogIn />} />
+        </Route>
       </Routes>
     </HashRouter>
   </React.StrictMode>
