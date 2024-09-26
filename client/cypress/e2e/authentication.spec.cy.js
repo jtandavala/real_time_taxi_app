1d61caa57bb4af8ede1d78ed60f341d7d987797d
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-09-26 06:27:51 +0100

    :sparkles: (SignUp) add formik to SignUp component

--- client/cypress/e2e/authentication.spec.cy.js
+++ client/cypress/e2e/authentication.spec.cy.js
@@ -2,9 +2,21 @@
 describe("Authentication", function () {
   it("Can log in.", function () {
     cy.visit("/#/log-in");
-    cy.get("input#username").type("gary.cole@example.com");
+    cy.get("input#username").type("bertha.price@example.com");
     cy.get("input#password").type("pAssw0rd", { log: false });
     cy.get("button").contains("Log in").click();
     cy.hash().should("eq", "#/");
+  });
+
+  it("Can sign up.", function () {
+    cy.visit("/#/sign-up");
+    cy.get("input#username").type("bertha.price@example.com");
+    cy.get("input#firstName").type("Bertha");
+    cy.get("input#lastName").type("Price");
+    cy.get("input#password").type("pAssw0rd", { log: false });
+    cy.get("select#group").select("driver");
+    cy.get("input#photo").attachFile("images/photo.jpg");
+    cy.get("button").contains("Sign up").click();
+    cy.hash().should("eq", "#/log-in");
   });
 });
